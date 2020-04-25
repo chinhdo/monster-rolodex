336cb1b71e5047992cb4e9b90e59ed1ea5485b46
Chinh Do <cdo@vienxu.com>
2020-04-25 15:00:09 -0400

    Updates

---
+++ precache-manifest.96d24138b7c325db012cab1092fdb2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc63b063da01d54a78c72cd683d66d29",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bad5de720f40ec31c2e6",
+    "url": "/monster-rolodex/static/css/main.03a102dc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad5de720f40ec31c2e6",
+    "url": "/monster-rolodex/static/js/main.bc492afb.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
